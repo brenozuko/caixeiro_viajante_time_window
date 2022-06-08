a18a655266ae3c9213b66a3883761567a6118004
rick-ribeiro <henrique.ribeiro@iclinic.com.br>
2022-06-08 05:49:56 -0300

    fix: fixing results

---
+++ dados/results/inst_10/inst_10.sol
@@ -0,0 +1,259 @@
+Problem:    Caixeiro viajante com deadline
+Rows:       107
+Columns:    120
+Non-zeros:  389
+Status:     OPTIMAL
+Objective:  0 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 R1           NS             1             1             =         < eps
+     2 R2           NS             1             1             =         < eps
+     3 R3           NS             1             1             =         < eps
+     4 R4           NS             1             1             =         < eps
+     5 R5           NS             1             1             =         < eps
+     6 R6           NS             1             1             =         < eps
+     7 R7           NS             1             1             =         < eps
+     8 R8           NS             1             1             =         < eps
+     9 R9           NS             1             1             =         < eps
+    10 R10          NS             1             1             =         < eps
+    11 R11          NS             1             1             =         < eps
+    12 R12          NS             1             1             =         < eps
+    13 R13          NS             1             1             =         < eps
+    14 R14          NS             1             1             =         < eps
+    15 R15          NS             1             1             =         < eps
+    16 R16          NS             1             1             =         < eps
+    17 R17          B              1             1             = 
+    18 R18          NS             1             1             =         < eps
+    19 R19          NL         -2806         -2806                       < eps
+    20 R20          B       -55.3941         -2806               
+    21 R21          B              0         -2806               
+    22 R22          B              0         -2806               
+    23 R23          B              0         -2806               
+    24 R24          B              0         -2806               
+    25 R25          B              0         -2806               
+    26 R26          B              0         -2806               
+    27 R27          B              0         -2806               
+    28 R28          B              0         -2806               
+    29 R29          B       -2769.24         -2806               
+    30 R30          B       -56.7767         -2806               
+    31 R31          B              0         -2806               
+    32 R32          B              0         -2806               
+    33 R33          B              0         -2806               
+    34 R34          B              0         -2806               
+    35 R35          B              0         -2806               
+    36 R36          B              0         -2806               
+    37 R37          B       -54.7763         -2806               
+    38 R38          B              0         -2806               
+    39 R39          NL         -2806         -2806                       < eps
+    40 R40          B       -53.2595         -2806               
+    41 R41          B              0         -2806               
+    42 R42          B              0         -2806               
+    43 R43          B              0         -2806               
+    44 R44          B              0         -2806               
+    45 R45          B              0         -2806               
+    46 R46          B              0         -2806               
+    47 R47          B              0         -2806               
+    48 R48          B              0         -2806               
+    49 R49          NL         -2806         -2806                       < eps
+    50 R50          B        -101.36         -2806               
+    51 R51          B              0         -2806               
+    52 R52          B              0         -2806               
+    53 R53          B              0         -2806               
+    54 R54          B              0         -2806               
+    55 R55          B              0         -2806               
+    56 R56          B              0         -2806               
+    57 R57          B       -53.8937         -2806               
+    58 R58          B              0         -2806               
+    59 R59          B          -2801         -2806               
+    60 R60          B         -48.97         -2806               
+    61 R61          B              0         -2806               
+    62 R62          B              0         -2806               
+    63 R63          B              0         -2806               
+    64 R64          B              0         -2806               
+    65 R65          B              0         -2806               
+    66 R66          B              0         -2806               
+    67 R67          B       -49.6095         -2806               
+    68 R68          B              0         -2806               
+    69 R69          NL         -2806         -2806                       < eps
+    70 R70          B              0         -2806               
+    71 R71          B       -9.56825         -2806               
+    72 R72          B              0         -2806               
+    73 R73          B              0         -2806               
+    74 R74          B              0         -2806               
+    75 R75          B              0         -2806               
+    76 R76          B              0         -2806               
+    77 R77          B              0         -2806               
+    78 R78          B              0         -2806               
+    79 R79          B       -2756.89         -2806               
+    80 R80          B       -81.4549         -2806               
+    81 R81          B              0         -2806               
+    82 R82          B              0         -2806               
+    83 R83          B              0         -2806               
+    84 R84          B              0         -2806               
+    85 R85          B              0         -2806               
+    86 R86          B              0         -2806               
+    87 R87          B       -9.32914         -2806               
+    88 R88          B              0         -2806               
+    89 R89          NL         -2806         -2806                       < eps
+    90 R90          B       -80.8941         -2806               
+    91 R91          B              0         -2806               
+    92 R92          B              0         -2806               
+    93 R93          B              0         -2806               
+    94 R94          B              0         -2806               
+    95 R95          B              0         -2806               
+    96 R96          B              0         -2806               
+    97 R97          B       -81.4339         -2806               
+    98 R98          B              0         -2806               
+    99 R99          B       -2755.79         -2806               
+   100 R100         B              0          -430               
+   101 R101         B              0           -17               
+   102 R102         B              0          -449               
+   103 R103         B              0          -490               
+   104 R104         B              0          -428               
+   105 R105         B              0          -535               
+   106 R106         B              0           -28               
+   107 R107         B              0          -419               
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x[0,0]       NL             0             0             1         < eps
+     2 x[0,1]       B        0.98059             0             1 
+     3 x[0,2]       B      0.0194101             0             1 
+     4 x[0,3]       NL             0             0             1         < eps
+     5 x[0,4]       NL             0             0             1         < eps
+     6 x[0,5]       NL             0             0             1         < eps
+     7 x[0,6]       NL             0             0             1         < eps
+     8 x[0,7]       NL             0             0             1         < eps
+     9 x[0,8]       NL             0             0             1         < eps
+    10 x[0,9]       NL             0             0             1         < eps
+    11 x[1,0]       NL             0             0             1         < eps
+    12 x[1,1]       NL             0             0             1         < eps
+    13 x[1,2]       B        0.98059             0             1 
+    14 x[1,3]       B      0.0194101             0             1 
+    15 x[1,4]       NL             0             0             1         < eps
+    16 x[1,5]       NL             0             0             1         < eps
+    17 x[1,6]       NL             0             0             1         < eps
+    18 x[1,7]       NL             0             0             1         < eps
+    19 x[1,8]       NL             0             0             1         < eps
+    20 x[1,9]       NL             0             0             1         < eps
+    21 x[2,0]       NL             0             0             1         < eps
+    22 x[2,1]       B      0.0194101             0             1 
+    23 x[2,2]       NL             0             0             1         < eps
+    24 x[2,3]       B       0.962143             0             1 
+    25 x[2,4]       B      0.0184471             0             1 
+    26 x[2,5]       NL             0             0             1         < eps
+    27 x[2,6]       NL             0             0             1         < eps
+    28 x[2,7]       NL             0             0             1         < eps
+    29 x[2,8]       NL             0             0             1         < eps
+    30 x[2,9]       NL             0             0             1         < eps
+    31 x[3,0]       NL             0             0             1         < eps
+    32 x[3,1]       NL             0             0             1         < eps
+    33 x[3,2]       NL             0             0             1         < eps
+    34 x[3,3]       NL             0             0             1         < eps
+    35 x[3,4]       B       0.964419             0             1 
+    36 x[3,5]       B      0.0355814             0             1 
+    37 x[3,6]       NL             0             0             1         < eps
+    38 x[3,7]       NL             0             0             1         < eps
+    39 x[3,8]       NL             0             0             1         < eps
+    40 x[3,9]       NL             0             0             1         < eps
+    41 x[4,0]       NL             0             0             1         < eps
+    42 x[4,1]       NL             0             0             1         < eps
+    43 x[4,2]       NL             0             0             1         < eps
+    44 x[4,3]       B      0.0184471             0             1 
+    45 x[4,4]       NL             0             0             1         < eps
+    46 x[4,5]       B       0.964419             0             1 
+    47 x[4,6]       B      0.0171343             0             1 
+    48 x[4,7]       NL             0             0             1         < eps
+    49 x[4,8]       NL             0             0             1         < eps
+    50 x[4,9]       NL             0             0             1         < eps
+    51 x[5,0]       NL             0             0             1         < eps
+    52 x[5,1]       NL             0             0             1         < eps
+    53 x[5,2]       NL             0             0             1         < eps
+    54 x[5,3]       NL             0             0             1         < eps
+    55 x[5,4]       B      0.0171343             0             1 
+    56 x[5,5]       NL             0             0             1         < eps
+    57 x[5,6]       B       0.979564             0             1 
+    58 x[5,7]       NL             0             0             1         < eps
+    59 x[5,8]       B     0.00330165             0             1 
+    60 x[5,9]       NL             0             0             1         < eps
+    61 x[6,0]       NL             0             0             1         < eps
+    62 x[6,1]       NL             0             0             1         < eps
+    63 x[6,2]       NL             0             0             1         < eps
+    64 x[6,3]       NL             0             0             1         < eps
+    65 x[6,4]       NL             0             0             1         < eps
+    66 x[6,5]       NL             0             0             1         < eps
+    67 x[6,6]       NL             0             0             1         < eps
+    68 x[6,7]       B         0.9719             0             1 
+    69 x[6,8]       B      0.0281003             0             1 
+    70 x[6,9]       NL             0             0             1         < eps
+    71 x[7,0]       NL             0             0             1         < eps
+    72 x[7,1]       NL             0             0             1         < eps
+    73 x[7,2]       NL             0             0             1         < eps
+    74 x[7,3]       NL             0             0             1         < eps
+    75 x[7,4]       NL             0             0             1         < eps
+    76 x[7,5]       NL             0             0             1         < eps
+    77 x[7,6]       B     0.00330165             0             1 
+    78 x[7,7]       NL             0             0             1         < eps
+    79 x[7,8]       B       0.968598             0             1 
+    80 x[7,9]       B      0.0281003             0             1 
+    81 x[8,0]       NL             0             0             1         < eps
+    82 x[8,1]       NL             0             0             1         < eps
+    83 x[8,2]       NL             0             0             1         < eps
+    84 x[8,3]       NL             0             0             1         < eps
+    85 x[8,4]       NL             0             0             1         < eps
+    86 x[8,5]       NL             0             0             1         < eps
+    87 x[8,6]       NL             0             0             1         < eps
+    88 x[8,7]       B      0.0281003             0             1 
+    89 x[8,8]       NL             0             0             1         < eps
+    90 x[8,9]       B         0.9719             0             1 
+    91 x[9,0]       NL             0             0             1         < eps
+    92 x[9,1]       NL             0             0             1         < eps
+    93 x[9,2]       NL             0             0             1         < eps
+    94 x[9,3]       NL             0             0             1         < eps
+    95 x[9,4]       NL             0             0             1         < eps
+    96 x[9,5]       NL             0             0             1         < eps
+    97 x[9,6]       NL             0             0             1         < eps
+    98 x[9,7]       NL             0             0             1         < eps
+    99 x[9,8]       NL             0             0             1         < eps
+   100 x[9,9]       NL             0             0             1         < eps
+   101 phi[0]       NL             0             0                       < eps
+   102 phi[1]       NL             0             0                       < eps
+   103 phi[2]       NL             0             0                       < eps
+   104 phi[3]       NL             0             0                       < eps
+   105 phi[4]       NL             0             0                       < eps
+   106 phi[5]       NL             0             0                       < eps
+   107 phi[6]       NL             0             0                       < eps
+   108 phi[7]       NL             0             0                       < eps
+   109 phi[8]       NL             0             0                       < eps
+   110 phi[9]       NL             0             0                       < eps
+   111 w[0]         NL             0             0                           1 
+   112 w[1]         NL             0             0                           1 
+   113 w[2]         NL             0             0                           1 
+   114 w[3]         NL             0             0                           1 
+   115 w[4]         NL             0             0                           1 
+   116 w[5]         NL             0             0                           1 
+   117 w[6]         NL             0             0                           1 
+   118 w[7]         NL             0             0                           1 
+   119 w[8]         NL             0             0                           1 
+   120 w[9]         NL             0             0                       < eps
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 2.20e-13 on row 37
+        max.rel.err = 6.00e-15 on row 71
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
